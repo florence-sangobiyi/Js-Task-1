b3848584fb84645ff22d9f22ca8f2cfa21655ea9
florence-sangobiyi <58138116+florence-sangobiyi@users.noreply.github.com>
2020-03-25 23:39:14 +0100

    display name and course

---
+++ .js
@@ -0,0 +1,18 @@
+var Name = 'Sangobiyi Titilayo'
+var courses = ["Javascript","Nodejs"];
+
+
+function DisplayEven (num) {
+    for  (let i = 1, i <200; i++ ) {
+        if (i % 2 === 0 ) {
+            console.log (i) ;
+        }
+    }
+}
+function DisplayOdd (num ){
+    for (let i =1 , i < 200 ; i++) {
+        if ( i % 3 ===0 ){
+            console.log(i);
+        }
+    }
+}
